a32e4b8b3c90b25c25b6be62e23daf45b49463e2
Zackery Kramer <zackkramer@Zackerys-MacBook-Air.local>
2021-09-21 21:05:20 -0400

    Second commit

--- api-project.js
+++ api-project.js
@@ -1,7 +1,14 @@
-const baseURL = 'https://pokeapi.co/api/v2/ability/{id or name}/';
-let url;
+const baseURL = 'https://pokeapi.co/api/v2/pokemon/ditto';
+let quoteContainer = document.querySelector('.quoteContainer');
 
 const searchTerm = document.querySelector('.search'); // Pulling a specific class name from the HTML, and assigning it to a variable.
 const name = document.querySelector('.name');
 const generation = document.querySelector('.generation');
 const effect = document.querySelector('effect');
+
+console.log(data)
+
+// waiting for id and name as it is a sample in the url
+// async/await or fetch like I did on repl 3 
+// 1. data to be console log
+// 2. then ready for display function which puts stuff on my page.
